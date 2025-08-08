eaa463f0cbe4911d4da22968122a581d227f0265
Praneeth Boddeti <bpraneet@asu.edu>
2025-08-08 13:55:37 -0400

    Project-1 Update

--- js/scripts.js
+++ js/scripts.js
@@ -51,9 +51,9 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
+    // Activate SimpleLightbox plugin for project items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: '#projects a.projects-box'
     });
 
 });
